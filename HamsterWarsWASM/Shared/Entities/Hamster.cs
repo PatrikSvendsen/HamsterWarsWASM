c5d297c1c9abb8f354399229faa9a97a20e26596
Patrik Svendsen <patrik.svendsen@outlook.com>
2022-05-06 18:19:07 +0200

    Updated UI. Updated delete for history. Needs to be checked. Fixed component for List

--- HamsterWarsWASM/Shared/Entities/Hamster.cs
+++ HamsterWarsWASM/Shared/Entities/Hamster.cs
@@ -8,13 +8,16 @@ public class Hamster
     [Key]
     [Column("HamsterId")]
     public int Id { get; set; }
-    [Required(ErrorMessage = "This field is required")]
+    [Required(ErrorMessage = "Name field is required")]
     [MaxLength(20)]
     public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Age field is required")]
     public int Age { get; set; }
     [MaxLength(20)]
+    [Required(ErrorMessage = "Food field is required")]
     public string FavFood { get; set; } = string.Empty;
     [MaxLength(20)]
+    [Required(ErrorMessage = "Loves field is required")]
     public string Loves { get; set; } = string.Empty;
     public string ImgName { get; set; } = string.Empty;
     public int Wins { get; set; } = 0;
